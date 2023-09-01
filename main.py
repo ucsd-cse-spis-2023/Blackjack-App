a6d14f4f1a68a6d8e4cf9107bb8e970b8f19a25c
Kayla-Phillips <k3phillips@ucsd.edu>
2023-08-31 21:26:21 -0700

    Added all of the card IDs

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+
+   return "Hello World!"
+
+@app.route("/game")
+def play():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+
+   app.run(host='0.0.0.0', port=3000)
